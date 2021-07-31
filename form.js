78404123bc28677f6b61c0555c6eababeb7cea42
tubi55 <tubi55@nate.com>
2021-07-31 14:52:49 +0900

    비밀번호 회원인증 함수 추가

--- form.js
+++ form.js
@@ -1,5 +1,6 @@
 $("input[type=submit]").on("click", e=>{   
     if(!isTxt("userid", 5)) e.preventDefault();
+    if(!isPwd("pwd1", "pwd2", 5)) e.preventDefault();
 });
 
 
@@ -9,6 +10,7 @@ function isTxt(name, len){
     const msg = $(`[name=${name}]`).attr("placeholder");
 
     if(txt==""){
+        alert(msg);
         $(`[name=${name}]`).addClass("error");
         return false;
     }else{
@@ -21,4 +23,62 @@ function isTxt(name, len){
             return true;
         }
     }
+}
+
+function isPwd(name1, name2, len){
+    const $pwd1 = $(`input[name=${name1}]`);
+    const $pwd2 = $(`input[name=${name2}]`);
+    const pwd1 = $pwd1.val();
+    const pwd2 = $pwd2.val();
+    let isConfirm = false;
+    let i=0;
+    const num = /[0-9]/;
+    const eng = /[a-zA-Z]/;
+    const spc = /[~!@#$%^&*()_+]/;
+
+    if(pwd1 === pwd2){
+        //비번이 len갯수보다 큰지 확인
+        if(pwd1.length >= len){
+            i++;
+        }else{
+            alert("비밀번호를 5자리 이상 입력하세요.");
+        }
+
+        //비번에 숫자가 포함되어 있는지 확인
+        if(num.test(pwd1)){
+            i++;
+        }else{
+            alert("비밀번호에 숫자를 포함하세요.");
+        }
+
+        //비번에 문자가 포함되어 있는지 확인
+        if(eng.test(pwd1)){
+            i++;
+        }else{
+            alert("비밀번호에 문자를 포함하세요.");
+        }
+
+        //비번에 특수문자가 포함되어 있는지 확인
+        if(spc.test(pwd1)){
+            i++;
+        }else{
+            alert("비밀번호에 특수문자를 포함하세요.")
+        }
+
+        if(i===4){
+            $pwd1.removeClass("error");
+            $pwd2.removeClass("error");
+            isConfirm = true;
+            return isConfirm;
+        }
+    }else{
+        $pwd1.addClass("error");
+        $pwd2.addClass("error");
+        return isConfirm;
+    }
+    //대전제. name1, name의 비번이 같아야됨
+    //조건1. name1의 갯수가 len보다 큼
+    //조건2. name1에 숫자포함
+    //조건3. name1에 문자표함
+    //조건4. name1에 특수문자 포함
 }
