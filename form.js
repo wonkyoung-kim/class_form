709a8327ee0ed0b03a03eb1637020c42383c1577
tubi55 <tubi55@nate.com>
2021-07-31 14:22:10 +0900

    텍스트 인증함수 적용

--- form.js
+++ form.js
@@ -1,16 +1,24 @@
-$("input[type=submit]").on("click", e=>{    
+$("input[type=submit]").on("click", e=>{   
+    if(!isTxt("userid", 5)) e.preventDefault();
+});
 
-    const txt = $("[name=userid]").val();
-    const msg = $("[name=userid]").attr("placeholder");
+
+function isTxt(name, len){
+    if(len == undefined) len = 5;
+    const txt = $(`[name=${name}]`).val();
+    const msg = $(`[name=${name}]`).attr("placeholder");
 
     if(txt==""){
-        e.preventDefault();
-        alert(msg);
+        $(`[name=${name}]`).addClass("error");
+        return false;
     }else{
-        if(txt.length < 5){
-            e.preventDefault();
-            alert("최소 5글자 이상 입력하세요!!");
-            $("[name=userid]").addClass("error");
+        if(txt.length < len){
+            alert(`최소 ${len}글자 이상 입력하세요!`);
+            $(`[name=${name}]`).addClass("error");
+            return false;
+        }else{
+            $(`[name=${name}]`).removeClass("error");
+            return true;
         }
     }
-})
+}
